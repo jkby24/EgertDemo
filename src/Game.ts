dc9b706b57d8de61a61db79217e47bb5b8208f3d
cgw <289929553@qq.com>
2017-07-13 19:28:38 +0800

    first commit

---
+++ src/Game.ts
@@ -0,0 +1,10 @@
+// TypeScript file
+/**
+ * name
+ */
+class Game extends eui.Component {
+    constructor() {
+        super();
+        this.skinName = "resource/eui_skins/GameScence.exml"
+    }
+}
